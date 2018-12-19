e8e8df946688b063010eb40dca4cae3d9c86fdea
Inspecode <inspecode-bot@rocro.com>
2018-12-19 01:39:51 +0000

    Fix Artistic Style errors

--- src/DpDisp/DpDisp.WPF/MainWindow.xaml.cs
+++ src/DpDisp/DpDisp.WPF/MainWindow.xaml.cs
@@ -16,86 +16,86 @@ using System.Windows.Shapes;
 
 namespace DpDisp.WPF
 {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
-    public partial class MainWindow : Window
+/// <summary>
+/// MainWindow.xaml の相互作用ロジック
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+    DpService _dp;
+    bool dploop = false;
+    /// <summary>
+    /// DPと接続
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void AppBarConnect_Click(object sender, RoutedEventArgs e)
+    {
+        if (dploop == false)
         {
-            InitializeComponent();
-        }
-        DpService _dp;
-        bool dploop = false;
-        /// <summary>
-        /// DPと接続
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void AppBarConnect_Click(object sender, RoutedEventArgs e)
-        {
+            // 接続
+            if (_dp == null)
+            {
+                _dp = new DpService();
+                if (await _dp.Connect() == false)
+                {
+                    _dp = null;
+                    return;
+                }
+            }
+            // 定期的に実行
             if (dploop == false)
             {
-                // 接続
-                if (_dp == null)
+                var t = new Task(async () =>
                 {
-                    _dp = new DpService();
-                    if (await _dp.Connect() == false)
+                    dploop = true;
+                    while (dploop)
                     {
-                        _dp = null;
-                        return;
+                        await Dispatcher.Invoke( async () => {
+                            image1.Source = await _dp.GetImage();
+                        });
+                        await Task.Delay(500);
                     }
-                }
-                // 定期的に実行
-                if (dploop == false)
-                {
-                    var t = new Task(async () =>
-                    {
-                        dploop = true;
-                        while (dploop)
-                        {
-                            await Dispatcher.Invoke( async () => {
-                                image1.Source = await _dp.GetImage();
-                            });
-                            await Task.Delay(500);
-                        }
-                    });
-                    t.Start();
-                }
-                btnConnect.IsChecked = true;
-                // btnConnect.Icon = new SymbolIcon(Symbol.Stop);
+                });
+                t.Start();
             }
-            else
-            {
-                // 停止
-                dploop = false;
-                btnConnect.IsChecked = false;
-                // btnConnect.Icon = new SymbolIcon(Symbol.Play);
-            }
+            btnConnect.IsChecked = true;
+            // btnConnect.Icon = new SymbolIcon(Symbol.Stop);
         }
-
-        /// <summary>
-        /// 画像保存
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void AppBarSave_Click(object sender, RoutedEventArgs e)
+        else
         {
-            var now = DateTime.Now;
-            var filename = "dpt-" + now.ToString("yyyy-MM-dd HHmmss") + ".jpg";
-            var wb = image1.Source as BitmapSource;
-
-            var folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "DpDisp";
-            if ( !System.IO.Directory.Exists(folder))
-            {
-                System.IO.Directory.CreateDirectory(folder );
-            }
-            var path = folder + "\\" + filename;
-
-            FileStream filestream = new FileStream(path, FileMode.Create);
-            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(wb));
-            encoder.Save(filestream);
+            // 停止
+            dploop = false;
+            btnConnect.IsChecked = false;
+            // btnConnect.Icon = new SymbolIcon(Symbol.Play);
         }
     }
+
+    /// <summary>
+    /// 画像保存
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void AppBarSave_Click(object sender, RoutedEventArgs e)
+    {
+        var now = DateTime.Now;
+        var filename = "dpt-" + now.ToString("yyyy-MM-dd HHmmss") + ".jpg";
+        var wb = image1.Source as BitmapSource;
+
+        var folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\" + "DpDisp";
+        if ( !System.IO.Directory.Exists(folder))
+        {
+            System.IO.Directory.CreateDirectory(folder );
+        }
+        var path = folder + "\\" + filename;
+
+        FileStream filestream = new FileStream(path, FileMode.Create);
+        JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+        encoder.Frames.Add(BitmapFrame.Create(wb));
+        encoder.Save(filestream);
+    }
+}
 }
--- src/DpDisp/DpDisp/MainPage.xaml.cs
+++ src/DpDisp/DpDisp/MainPage.xaml.cs
@@ -22,135 +22,135 @@ using Windows.UI.Xaml.Navigation;
 
 namespace DpDisp
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
-    public sealed partial class MainPage : Page
+/// <summary>
+/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+/// </summary>
+public sealed partial class MainPage : Page
+{
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
+    }
 
-        DpService _dp;
-        bool dploop = false;
-        /// <summary>
-        /// DPと接続
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void AppBarConnect_Click(object sender, RoutedEventArgs e)
+    DpService _dp;
+    bool dploop = false;
+    /// <summary>
+    /// DPと接続
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void AppBarConnect_Click(object sender, RoutedEventArgs e)
+    {
+        if ( dploop == false )
         {
-            if ( dploop == false )
+            // 接続
+            if (_dp == null)
             {
-                // 接続
-                if (_dp == null)
+                _dp = new DpService();
+                if (await _dp.Connect() == false)
                 {
-                    _dp = new DpService();
-                    if (await _dp.Connect() == false)
+                    _dp = null;
+                    return;
+                }
+            }
+            // 定期的に実行
+            if (dploop == false)
+            {
+                var t = new Task(async () =>
+                {
+                    dploop = true;
+                    while (dploop)
                     {
-                        _dp = null;
-                        return;
+                        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () => {
+                            image1.Source = await _dp.GetImage();
+                        });
+                        await Task.Delay(500);
                     }
-                }
-                // 定期的に実行
-                if (dploop == false)
-                {
-                    var t = new Task(async () =>
-                    {
-                        dploop = true;
-                        while (dploop)
-                        {
-                            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () => {
-                                image1.Source = await _dp.GetImage();
-                            });
-                            await Task.Delay(500);
-                        }
-                    });
-                    t.Start();
-                }
-                btnConnect.Icon = new SymbolIcon(Symbol.Stop);
+                });
+                t.Start();
             }
-            else
-            {
-                // 停止
-                dploop = false;
-                btnConnect.Icon = new SymbolIcon(Symbol.Play);
-            }
+            btnConnect.Icon = new SymbolIcon(Symbol.Stop);
         }
-
-        /// <summary>
-        /// 画像保存
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void AppBarSave_Click(object sender, RoutedEventArgs e)
+        else
         {
-            var now = DateTime.Now;
-            var filename = "dpt-" + now.ToString("yyyy-MM-dd HHmmss") + ".jpg";
-
-            var wb = image1.Source as WriteableBitmap;
-            SoftwareBitmap outputBitmap = SoftwareBitmap.CreateCopyFromBuffer( wb.PixelBuffer, BitmapPixelFormat.Bgra8, wb.PixelWidth, wb.PixelHeight );
-
-            StorageFolder storageFolder = KnownFolders.PicturesLibrary;
-            StorageFolder folder;
-            try
-            {
-                folder = await storageFolder.GetFolderAsync("DpDisp");
-            }
-            catch
-            {
-                await storageFolder.CreateFolderAsync("DpDisp");
-                folder = await storageFolder.GetFolderAsync("DpDisp");
-            }
-
-            StorageFile file = await folder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
-            SaveSoftwareBitmapToFile(outputBitmap, file);
-        }
-
-
-        private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
-        {
-            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
-            {
-                // Create an encoder with the desired format
-                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
-
-                // Set the software bitmap
-                encoder.SetSoftwareBitmap(softwareBitmap);
-
-                // Set additional encoding parameters, if needed
-                encoder.BitmapTransform.ScaledWidth = 1200;
-                encoder.BitmapTransform.ScaledHeight = 1600;
-                // encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
-                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Fant;
-                encoder.IsThumbnailGenerated = true;
-
-                try
-                {
-                    await encoder.FlushAsync();
-                }
-                catch (Exception err)
-                {
-                    switch (err.HResult)
-                    {
-                        case unchecked((int)0x88982F81): //WINCODEC_ERR_UNSUPPORTEDOPERATION
-                                                         // If the encoder does not support writing a thumbnail, then try again
-                                                         // but disable thumbnail generation.
-                            encoder.IsThumbnailGenerated = false;
-                            break;
-                        default:
-                            throw err;
-                    }
-                }
-
-                if (encoder.IsThumbnailGenerated == false)
-                {
-                    await encoder.FlushAsync();
-                }
-
-
-            }
+            // 停止
+            dploop = false;
+            btnConnect.Icon = new SymbolIcon(Symbol.Play);
         }
     }
+
+    /// <summary>
+    /// 画像保存
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void AppBarSave_Click(object sender, RoutedEventArgs e)
+    {
+        var now = DateTime.Now;
+        var filename = "dpt-" + now.ToString("yyyy-MM-dd HHmmss") + ".jpg";
+
+        var wb = image1.Source as WriteableBitmap;
+        SoftwareBitmap outputBitmap = SoftwareBitmap.CreateCopyFromBuffer( wb.PixelBuffer, BitmapPixelFormat.Bgra8, wb.PixelWidth, wb.PixelHeight );
+
+        StorageFolder storageFolder = KnownFolders.PicturesLibrary;
+        StorageFolder folder;
+        try
+        {
+            folder = await storageFolder.GetFolderAsync("DpDisp");
+        }
+        catch
+        {
+            await storageFolder.CreateFolderAsync("DpDisp");
+            folder = await storageFolder.GetFolderAsync("DpDisp");
+        }
+
+        StorageFile file = await folder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+        SaveSoftwareBitmapToFile(outputBitmap, file);
+    }
+
+
+    private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+    {
+        using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+        {
+            // Create an encoder with the desired format
+            BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+
+            // Set the software bitmap
+            encoder.SetSoftwareBitmap(softwareBitmap);
+
+            // Set additional encoding parameters, if needed
+            encoder.BitmapTransform.ScaledWidth = 1200;
+            encoder.BitmapTransform.ScaledHeight = 1600;
+            // encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
+            encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Fant;
+            encoder.IsThumbnailGenerated = true;
+
+            try
+            {
+                await encoder.FlushAsync();
+            }
+            catch (Exception err)
+            {
+                switch (err.HResult)
+                {
+                case unchecked((int)0x88982F81): //WINCODEC_ERR_UNSUPPORTEDOPERATION
+                    // If the encoder does not support writing a thumbnail, then try again
+                    // but disable thumbnail generation.
+                    encoder.IsThumbnailGenerated = false;
+                    break;
+                default:
+                    throw err;
+                }
+            }
+
+            if (encoder.IsThumbnailGenerated == false)
+            {
+                await encoder.FlushAsync();
+            }
+
+
+        }
+    }
+}
 }
